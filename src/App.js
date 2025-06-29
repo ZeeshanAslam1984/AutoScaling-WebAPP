536573692bf8b863a1102beeb9cbb8a5de7a2f82
ZeeshanAslam1984 <zesh1984@gmail.com>
2025-06-30 00:19:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          sucessfully mmade the project
+          Sucessfully made the project.
         </p>
         <a
           className="App-link"
